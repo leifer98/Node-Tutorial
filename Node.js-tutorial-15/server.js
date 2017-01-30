a5a83e1dd08f9d92e544814c3d129f721fa91f6e
ofri <ofriofriofri@gmail.com>
2017-01-30 04:19:44 +0200

    tutorials 15-22

---
+++ Node.js-tutorial-15/server.js
@@ -0,0 +1,30 @@
+var connect = require('connect');
+var http = require('http');
+
+var app = connect();
+
+// function doFirst(request, response, next) {
+//     console.log("Bacon!");
+//     //next();
+// }
+// function doSecond(request, response, next) {
+//     console.log("Tona!");
+//     next();
+// }
+//
+//
+// app.use(doFirst);
+// app.use(doSecond);
+
+function profile(request, response) {
+    console.log("User requested profile");
+}
+function forum(request, response) {
+    console.log("User requested forum");
+}
+
+app.use('/profile',profile);
+app.use('/forum',forum);
+
+http.createServer(app).listen(8888);
+console.log("Server is now running...");
